0be2d7634c9defcdb25f86ac31439677765037e1
unknown <srbhsamir6@gmail.com>
2023-08-22 21:13:43 +0530

    1st assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args){
+        Product p = new Product();
+        p.product(10,15);
+        p.product(10,15,20);
+        p.product(5.3,6.4);
+    }
 
 }
