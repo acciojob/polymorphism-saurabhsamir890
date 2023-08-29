e60cdca993845ae3e6f984730aa1c94649941b24
unknown <srbhsamir6@gmail.com>
2023-08-29 13:16:04 +0530

    Attempt100

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,30 +2,26 @@ package com.driver;
 
 
 
-class Product {
-    public Product() {
-    }
 
-    public int product(int x, int y) {
-        return x*y;
-
-    }
-    public int product(int x, int y, int z){
-        return x*y*z;
-    }
-    public double product(double x, double y){
-        return x*y;
-    }
-}
 
 public class Main {
-    public static void main(String[] args){
-        Product p = new Product();
+    public static class Product {
+        public int product(int x, int y) {
+            return x * y;
+        }
 
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
 
-        p.product(10,15);
-        p.product(10,15,20);
-        p.product(5.3,2.5);
+        public double product(double x, double y) {
+            return x * y;
+        }
     }
 
+    public static void main(String[] args) {
+        Product p = new Product();
+        System.out.println(p.product(20, 40));
+        System.out.println(p.product(10, 20));
+    }
 }
