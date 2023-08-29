b469b59ae242bcbf82ac6763e9402bcdf7662cd6
unknown <srbhsamir6@gmail.com>
2023-08-29 13:09:13 +0530

    Attempt new

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,8 +1,27 @@
 package com.driver;
 
+
+
+class Product {
+    public Product() {
+    }
+
+    public int product(int x, int y) {
+        return x*y;
+
+    }
+    public int product(int x, int y, int z){
+        return x*y*z;
+    }
+    public double product(double x, double y){
+        return x*y;
+    }
+}
+
 public class Main {
     public static void main(String[] args){
         Product p = new Product();
+
 
         p.product(10,15);
         p.product(10,15,20);
